5179c50b0fbac1cd77b7b7dc943d585670fb1fd3
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2021-11-28 23:46:46 +0300

    create sign-up page

---
+++ pages/sign-up.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import { render } from 'react-dom';
+
+export default SignUp = () => {
+    render(
+        <div className="container">
+            <Head>
+                <title>Sign Up Kanji List</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        </div>
+    );
+};
